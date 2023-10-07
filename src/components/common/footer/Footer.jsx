8d7388821839bc68f1d4c8d72fd30ef7556f838b
chojeongeun <juy0816@naver.com>
2023-10-07 14:45:13 +0900

    footer styling

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,7 +1,24 @@
+import { Link } from 'react-router-dom';
+import { FaYoutube, FaTwitter } from 'react-icons/fa6';
+import './Footer.scss';
+
 export default function Footer() {
 	return (
 		<footer>
-			<h1>Footer</h1>
+			<h1>Positive Vibes</h1>
+			<p>2023 PV &copy; All Rights Reserved.</p>
+			<ul>
+				<li>
+					<Link to='/'>
+						<FaYoutube size={20} color={'red'} />
+					</Link>
+				</li>
+				<li>
+					<Link to='/'>
+						<FaTwitter size={20} color={'skyblue'} />
+					</Link>
+				</li>
+			</ul>
 		</footer>
 	);
 }
