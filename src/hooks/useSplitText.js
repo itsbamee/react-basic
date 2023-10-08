ada792784593f6aa3250e8764b02b40beb8a1606
chojeongeun <juy0816@naver.com>
2023-10-08 15:18:31 +0900

    return함수에 추가 파라미터 연결

---
+++ src/hooks/useSplitText.js
@@ -0,0 +1,13 @@
+export function useSplitText(ref) {
+	return (ref, interval = 0, delay = 0) => {
+		let text = ref.current.innerText;
+		let tags = '';
+		let count = 0;
+
+		for (let letter of text) {
+			tags += `<span style='display:inline-block; transition-delay:${interval * count + delay}s'>${letter}</span>`;
+			count++;
+		}
+		ref.current.innerHTML = tags;
+	};
+}
