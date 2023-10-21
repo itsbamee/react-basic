473461255a373943d3662b88e5e5c1125a964d3b
chojeongeun <juy0816@naver.com>
2023-10-21 13:04:53 +0900

    갤러리출력

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,39 +1,40 @@
 import Layout from '../../common/layout/Layout';
+import './Gallery.scss';
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
-export default function Youtube() {
+export default function Gallery() {
 	const [Pics, setPics] = useState([]);
+	console.log(Pics);
 
-	const fetchFlicker = async () => {
-		const api_key = 'AIzaSyDC60bIIkAJFzy7ji4a0Eo3AX6tYudhe1w';
-		const pid = 'PLYOPkdUKSFgWqafuDQN9di3uLJoTV3L3W';
-		const num = 10;
-		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
-		const data = await fetch(baseURL);
-		const json = data.json();
+	const fetchFlickr = async () => {
+		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
+		const key = '2a1a0aebb34012a99c23e13b49175343';
+		const method_interest = 'flickr.interestingness.getList';
+		const num = 40;
+		const url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
+		const data = await fetch(url);
+		const json = await data.json();
 		setPics(json.photos.photo);
 	};
 
 	useEffect(() => {
-		fetchFlicker();
+		fetchFlickr();
 	}, []);
 
 	return (
-		<Layout title={'Youtube'}>
-			{Pics.map((data, idx) => {
-				return (
-					<article key={idx}>
-						<h2>{data.snippet.title}</h2>
-						<p>{data.snippet.description}</p>
-						<div className='pic'>
-							<Link to={`/detail/${data.id}`}>
-								<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
-							</Link>
-						</div>
-					</article>
-				);
-			})}
+		<Layout title={'Gallery'}>
+			<div className='frame'>
+				{Pics.map((pic, idx) => {
+					return (
+						<article key={idx}>
+							<div className='pic'>
+								<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={pic.title} />
+							</div>
+							<h2>{pic.title}</h2>
+						</article>
+					);
+				})}
+			</div>
 		</Layout>
 	);
 }
