564c121596cb8623774093d53a83a2d049ce2555
chojeongeun <juy0816@naver.com>
2023-10-21 12:48:33 +0900

    fetch 문 일반 함수로 다시 변경

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,28 +1,21 @@
+import './Department.scss';
 import Layout from '../../common/layout/Layout';
 import React, { useEffect, useState } from 'react';
-import './Department.scss';
-import { useFetch } from '../../../hooks/useFetch';
-
-/*
-	React에서 외부 데이터 fetching 작업 흐름 (공식으로 외워라)
-	1. 컴포넌트 함수가 호출되고 외부데이터를 받을 state 초기화
-	2. 의존성 배열이 비어있는 useEffect hook 안쪽에서 fetch 함수로 비동기 데이터를 받은 다음에 state에 넘겨줌
-	3. useEffect 안쪽에서 fetch문을 써야되는 이유는 fetch 자체가 (fetch 자체가 클라이언트 기반으로 호출되는 데이터 방식) web api를 통해서 클라이언트 기반으로 데이터를 전달받는 방식이기 때문에, 무조건 컴포넌트가 마운트 되야지만 호출 가능
-	4. useEffect에 의해서 데이터가 받아지고 state에 전달되면 자동으로 컴포넌트는 재렌더링됨
-	5. 비동기 데이터를 활용해서 실제 JSX로 동적 DOM을 생성하는 시점은 2번째 렌더링 타임
-*/
 
 const path = process.env.PUBLIC_URL;
 
 export default function Department() {
-	const [Title, setTitle] = useState('');
 	const [Department, setDepartment] = useState([]);
 	const [History, setHistory] = useState([]);
-	const fetchData = useFetch();
 
 	useEffect(() => {
-		fetchData(`${path}/DB/Department.json`, setDepartment, setTitle);
-		fetchData(`${path}/DB/history.json`, setHistory);
+		fetch(`${path}/DB/history.json`)
+			.then((data) => data.json())
+			.then((json) => setHistory(json.history));
+
+		fetch(`${path}/DB/department.json`)
+			.then((data) => data.json())
+			.then((json) => setDepartment(json.members));
 	}, []);
 
 	return (
@@ -33,11 +26,11 @@ export default function Department() {
 					{History.map((data, idx) => {
 						return (
 							<React.Fragment key={idx}>
-								<h2>{Object.keys(data)[0]}</h2>
+								<h3>{Object.keys(data)[0]}</h3>
 								<ul>
-									{Object.values(data)[0].map((val, idx) => {
-										return <li key={idx}>{val}</li>;
-									})}
+									{Object.values(data)[0].map((val, idx) => (
+										<li key={idx}>{val}</li>
+									))}
 								</ul>
 							</React.Fragment>
 						);
@@ -46,16 +39,17 @@ export default function Department() {
 			</section>
 
 			<section id='memberBox'>
-				<h2>{Title.charAt(0).toUpperCase() + Title.slice(1)}</h2>
+				<h2>Department</h2>
+
 				<div className='con'>
 					{Department.map((member, idx) => {
 						return (
 							<article key={idx}>
 								<div className='pic'>
-									<img src={`${path}/img/${member.pic}`} alt='{member.pic}' />
-									<h3>{member.name}</h3>
-									<p>{member.position}</p>
+									<img src={`${path}/img/${member.pic}`} alt={member.name} />
 								</div>
+								<h3>{member.name}</h3>
+								<p>{member.position}</p>
 							</article>
 						);
 					})}
--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,9 +1,35 @@
 import Layout from '../../common/layout/Layout';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Gallery() {
+export default function Youtube() {
+	const [Vids, setVids] = useState([]);
+
+	useEffect(() => {
+		const api_key = 'AIzaSyDC60bIIkAJFzy7ji4a0Eo3AX6tYudhe1w';
+		const pid = 'PLYOPkdUKSFgWqafuDQN9di3uLJoTV3L3W';
+		const num = 10;
+		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+		fetch(baseURL)
+			.then((data) => data.json())
+			.then((json) => setVids(json.items));
+	}, []);
+
 	return (
-		<Layout title={'Gallery'}>
-			<p>갤러리 상세페이지</p>
+		<Layout title={'Youtube'}>
+			{Vids.map((data, idx) => {
+				return (
+					<article key={idx}>
+						<h2>{data.snippet.title}</h2>
+						<p>{data.snippet.description}</p>
+						<div className='pic'>
+							<Link to={`/detail/${data.id}`}>
+								<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+							</Link>
+						</div>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
