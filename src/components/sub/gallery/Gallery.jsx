b93b02e2f90e489b6805b7ade5f8fe13f3b636ad
chojeongeun <juy0816@naver.com>
2023-10-28 12:59:57 +0900

    검색어 입력창 생성 및 검색어 입력으로 데이터 호출

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -2,17 +2,19 @@ import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 import Masonry from 'react-masonry-component';
 import { useState, useEffect, useRef } from 'react';
+import { LuSearch } from 'react-icons/lu';
 
 export default function Gallery() {
-	const myID = '197119297@N02';
 	const [Pics, setPics] = useState([]);
-	let [IsUser, setIsUser] = useState(myID);
+	const [IsUser, setIsUser] = useState(true);
 	const refElBtnSet = useRef(null);
+	const myId = '199361154@N05';
+	const refElInput = useRef(null);
 
 	const fetchFlickr = async (opt) => {
 		console.log('fetching again...');
 		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
-		const key = process.env.REACT_APP_FLICKR_KEY;
+		const key = process.env.REACT_APP_FLICKER_KEY;
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
 		const method_search = 'flickr.photos.search';
@@ -39,32 +41,35 @@ export default function Gallery() {
 
 	const handleClickInterest = (e) => {
 		if (e.target.classList.contains('on')) return;
-		//inertestGallery함수가 호출시 IsUser값을 빈문자열 처리 (falsy)
-		setIsUser('');
+		setIsUser(false);
 		activateBtn(e);
 		fetchFlickr({ type: 'interest' });
 	};
 
 	const handleClickMine = (e) => {
-		//마이갤러리 함수 호출시에는 IsUser의 문자값이 담겨있더라도 내아이디값이랑 똑같지 않으면 핸들러 호출함
-		//다른 사용자 갤러리를 갔다가 My Gallery 함수 호출시 이미 IsUser값이 담겨있기 때문에 해당 함수가 호출되지 않는 문제 해결위함
-		if (e.target.classList.contains('on') || IsUser === myID) return;
-		setIsUser(myID);
+		if (e.target.classList.contains('on') && IsUser) return;
+		setIsUser(true);
 		activateBtn(e);
-		fetchFlickr({ type: 'user', id: myID });
+		fetchFlickr({ type: 'user', id: myId });
 	};
 
 	const handleClickUser = (e) => {
-		//IsUser값이 있기만 하면 핸들러함수 호출 중지
 		if (IsUser) return;
-		setIsUser(e.target.innerText);
+		setIsUser(true);
 		activateBtn(e);
 		fetchFlickr({ type: 'user', id: e.target.innerText });
 	};
 
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const tags = refElInput.current.value;
+		setIsUser('');
+		activateBtn(e);
+		fetchFlickr({ type: 'search', keyword: tags });
+	};
+
 	useEffect(() => {
-		fetchFlickr({ type: 'user', id: myID });
-		//fetchFlickr({ type: 'search', keyword: 'ocean' });
+		fetchFlickr({ type: 'user', id: myId });
 	}, []);
 
 	return (
@@ -76,6 +81,11 @@ export default function Gallery() {
 						My Gallery
 					</button>
 				</nav>
+
+				<form onSubmit={handleSubmit}>
+					<input type='text' placeholder='Search' ref={refElInput} />
+					<LuSearch className='btnSearch' fontSize={20} color={'#bbb'} />
+				</form>
 			</article>
 
 			<div className='frame'>
@@ -88,7 +98,6 @@ export default function Gallery() {
 										<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_w.jpg`} alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} />
 									</div>
 									<h2>{pic.title}</h2>
-
 									<div className='profile'>
 										<img
 											src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
