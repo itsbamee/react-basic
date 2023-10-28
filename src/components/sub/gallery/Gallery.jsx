440cd07a9509dfd9df9440984e501063673fbfc2
chojeongeun <juy0816@naver.com>
2023-10-28 09:00:15 +0900

    검색어 기능 추가

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -4,24 +4,27 @@ import Masonry from 'react-masonry-component';
 import { useState, useEffect, useRef } from 'react';
 
 export default function Gallery() {
+	const myID = '197119297@N02';
 	const [Pics, setPics] = useState([]);
-	const [IsUser, setIsUser] = useState(true);
+	let [IsUser, setIsUser] = useState(myID);
 	const refElBtnSet = useRef(null);
-	const myId = '199361154@N05';
 
 	const fetchFlickr = async (opt) => {
 		console.log('fetching again...');
 		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
-		const key = process.env.REACT_APP_FLICKER_KEY;
+		const key = process.env.REACT_APP_FLICKR_KEY;
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
+		const method_search = 'flickr.photos.search';
 		const num = 40;
 		let url = '';
 		const url_interest = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
 		const url_user = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.id}`;
+		const url_search = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.keyword}`;
 
 		opt.type === 'user' && (url = url_user);
 		opt.type === 'interest' && (url = url_interest);
+		opt.type === 'search' && (url = url_search);
 
 		const data = await fetch(url);
 		const json = await data.json();
@@ -36,27 +39,32 @@ export default function Gallery() {
 
 	const handleClickInterest = (e) => {
 		if (e.target.classList.contains('on')) return;
-		setIsUser(false);
+		//inertestGallery함수가 호출시 IsUser값을 빈문자열 처리 (falsy)
+		setIsUser('');
 		activateBtn(e);
 		fetchFlickr({ type: 'interest' });
 	};
 
 	const handleClickMine = (e) => {
-		if (e.target.classList.contains('on') && IsUser) return;
-		setIsUser(true);
+		//마이갤러리 함수 호출시에는 IsUser의 문자값이 담겨있더라도 내아이디값이랑 똑같지 않으면 핸들러 호출함
+		//다른 사용자 갤러리를 갔다가 My Gallery 함수 호출시 이미 IsUser값이 담겨있기 때문에 해당 함수가 호출되지 않는 문제 해결위함
+		if (e.target.classList.contains('on') || IsUser === myID) return;
+		setIsUser(myID);
 		activateBtn(e);
-		fetchFlickr({ type: 'user', id: myId });
+		fetchFlickr({ type: 'user', id: myID });
 	};
 
 	const handleClickUser = (e) => {
+		//IsUser값이 있기만 하면 핸들러함수 호출 중지
 		if (IsUser) return;
-		setIsUser(true);
+		setIsUser(e.target.innerText);
 		activateBtn(e);
 		fetchFlickr({ type: 'user', id: e.target.innerText });
 	};
 
 	useEffect(() => {
-		fetchFlickr({ type: 'user', id: myId });
+		fetchFlickr({ type: 'user', id: myID });
+		//fetchFlickr({ type: 'search', keyword: 'ocean' });
 	}, []);
 
 	return (
@@ -80,6 +88,7 @@ export default function Gallery() {
 										<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_w.jpg`} alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} />
 									</div>
 									<h2>{pic.title}</h2>
+
 									<div className='profile'>
 										<img
 											src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
