87b05ca4dc5b05ab56d0fe334e8b007656287a34
chojeongeun <juy0816@naver.com>
2023-10-29 13:53:33 +0900

    글저장, 글호출 완료

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -1,45 +1,57 @@
-import Layout from '../../common/layout/Layout';
 import './Community.scss';
-import { useRef } from 'react';
+import Layout from '../../common/layout/Layout';
 import { TfiWrite } from 'react-icons/tfi';
-import { ImCancelCircle } from 'react-icons/im';
+import { RxReset } from 'react-icons/rx';
+import { useRef, useState } from 'react';
 
 function Community() {
 	const refInput = useRef(null);
 	const refTextarea = useRef(null);
+	const [Posts, setPosts] = useState([]);
+	console.log(Posts);
+
+	const resetPost = () => {
+		refInput.current.value = '';
+		refTextarea.current.value = '';
+	};
+
+	const createPost = () => {
+		if (!refInput.current.value.trim() || !refTextarea.current.value.trim()) {
+			resetPost();
+			return alert('제목과 본문을 모두 입력하세요.');
+		}
+		setPosts([{ title: refInput.current.value, content: refTextarea.current.value }, ...Posts]);
+		resetPost();
+	};
 
 	return (
 		<Layout title={'Community'}>
 			<div className='wrap'>
 				<div className='inputBox'>
-					<input type='text' placeholder='title' ref={refInput} />
-					<textarea cols='30' rows='3' placeholder='leave message' ref={refTextarea}></textarea>
+					<input type='text' placeholder='Write Title' ref={refInput} />
+					<textarea cols='30' rows='5' placeholder='Write Content Message' ref={refTextarea}></textarea>
+
 					<nav>
-						<button>
-							<ImCancelCircle fontSize={20} color={'#555'} />
+						<button onClick={resetPost}>
+							<RxReset fontSize={20} color={'#555'} />
 						</button>
-						<button>
+						<button onClick={createPost}>
 							<TfiWrite fontSize={20} color={'#555'} />
 						</button>
 					</nav>
 				</div>
-				<div className='showBox'></div>
+
+				<div className='showBox'>
+					{Posts.map((post, idx) => (
+						<article key={idx}>
+							<h2>{post.title}</h2>
+							<p>{post.content}</p>
+						</article>
+					))}
+				</div>
 			</div>
 		</Layout>
 	);
 }
 
-/*
-  C (create : 글작성) "POST"
-  R (read : 글 불러오기) "GET"
-  U (update : 글 수정) "PUT"
-  D (delete : 글 삭제) "DELETE"
-
-  RESTful API : DB의 데이이터를 구조적으로 변경하기 위한 개발 방법론
-
-  로컬저장소(LocalStorage)
-  - 모든 브라우저가 내장하고 있는 경량의 저장공간
-  - 문자값만 저장가능 (5MB)
-  - 객체값을 문자화시켜서 저장
-  - 로컬저장소 값을 불러올때는 반대로 JSON형태로 객체로 parsing해서 가져와야 함
-*/
+export default Community;
