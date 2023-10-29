bfecb2328bad19f2cbd9756ae59d8d6bb8eedc98
chojeongeun <juy0816@naver.com>
2023-10-29 12:48:07 +0900

    community 컴포넌트 추가

---
+++ src/components/sub/community/Community.jsx
@@ -0,0 +1,8 @@
+import Layout from '../../common/layout/Layout';
+import './Community.scss';
+
+function Community() {
+	return <Layout title={'Community'}>Community</Layout>;
+}
+
+export default Community;
