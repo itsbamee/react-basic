f4498d41ffc943756f2f89225fe6202068517b4e
chojeongeun <juy0816@naver.com>
2023-10-29 13:21:03 +0900

    crud 주석 정리

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -29,4 +29,17 @@ function Community() {
 	);
 }
 
-export default Community;
+/*
+  C (create : 글작성) "POST"
+  R (read : 글 불러오기) "GET"
+  U (update : 글 수정) "PUT"
+  D (delete : 글 삭제) "DELETE"
+
+  RESTful API : DB의 데이이터를 구조적으로 변경하기 위한 개발 방법론
+
+  로컬저장소(LocalStorage)
+  - 모든 브라우저가 내장하고 있는 경량의 저장공간
+  - 문자값만 저장가능 (5MB)
+  - 객체값을 문자화시켜서 저장
+  - 로컬저장소 값을 불러올때는 반대로 JSON형태로 객체로 parsing해서 가져와야 함
+*/
