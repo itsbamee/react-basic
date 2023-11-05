da2a76bc75a947b9423734bcbe89c041aa040d3c
itsbamme <jan9239@naver.com>
2023-11-05 13:08:14 +0900

    인증할 항목의 초기값을 state에 저장

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Members.scss';
+import { useState, useRef, useEffect } from 'react';
 
 export default function Members() {
+	const initVal = useRef({
+		userid: '',
+		email: '',
+		pwd1: '',
+		pdw2: '',
+		edu: '',
+		gender: '',
+		interest: [],
+		comments: '',
+	});
+	const [Val, setVal] = useState(initVal.current);
 	return (
 		<Layout title={'Members'}>
 			<div className='wrap'>
@@ -16,54 +29,62 @@ export default function Members() {
 								<tbody>
 									<tr>
 										<td>
-											<input type='text' name='userid' placeholder='userId' />
+											<input type='text' name='userid' placeholder='UserId' />
 										</td>
 										<td>
-											<input type='text' name='email' placeholder='email' />
+											<input type='text' name='email' placeholder='Email' />
 										</td>
 									</tr>
 									<tr>
 										<td>
-											<input type='password' name='pwd1' placeholder='password' />
+											<input type='password' name='pwd1' placeholder='Password' />
 										</td>
 										<td>
-											<input type='password' name='pwd2' placeholder='re-password' />
+											<input type='password' name='pwd2' placeholder='Re-password' />
 										</td>
 									</tr>
 
 									<tr>
 										<td colSpan='2'>
 											<select name='edu'>
-												<option value=''>Education</option>
-												<option value='elementary-school'>초등학교 졸업</option>
-												<option value='middle-school'>중학교 졸업</option>
-												<option value='high-school'>고등학교 졸업</option>
-												<option value='college'>대학교 졸업</option>
+												<option defaultValue='defaultValue'>Education</option>
+												<option defaultValue='elementary-school'>초등학교 졸업</option>
+												<option defaultValue='middle-school'>중학교 졸업</option>
+												<option defaultValue='high-school'>고등학교 졸업</option>
+												<option defaultValue='college'>대학교 졸업</option>
 											</select>
 										</td>
 									</tr>
 									<tr>
 										<td colSpan='2'>
-											<input type='radio' value='female' id='female' name='gender' />
+											<input type='radio' defaultValue='female' id='female' name='gender' />
 											<label htmlFor='female'>Female</label>
 
-											<input type='radio' value='male' id='male' name='gender' />
+											<input type='radio' defaultValue='male' id='male' name='gender' />
 											<label htmlFor='male'>Male</label>
 										</td>
 									</tr>
 									<tr>
 										<td colSpan='2'>
 											<input type='checkbox' name='interest' id='sports' />
-											<label htmlFor='sports'>Sports</label>
+											<label htmlFor='sports' defaultValue='sports'>
+												Sports
+											</label>
 
 											<input type='checkbox' name='interest' id='reading' />
-											<label htmlFor='reading'>Reading</label>
+											<label htmlFor='reading' defaultValue='reading'>
+												Reading
+											</label>
 
 											<input type='checkbox' name='interest' id='music' />
-											<label htmlFor='music'>Music</label>
+											<label htmlFor='music' defaultValue='music'>
+												Music
+											</label>
 
 											<input type='checkbox' name='interest' id='game' />
-											<label htmlFor='game'>Game</label>
+											<label htmlFor='game' defaultValue='game'>
+												Game
+											</label>
 										</td>
 									</tr>
 									<tr>
